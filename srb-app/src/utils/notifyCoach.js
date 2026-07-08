0d913ee46cd68360910265901d47875f02ddb556
sacredrebellionbarbell <sacredrebellionbarbell@gmail.com>
2026-07-08 11:45:13 -0500

    Update notifyCoach.js

--- srb-app/src/utils/notifyCoach.js
+++ srb-app/src/utils/notifyCoach.js
@@ -1,9 +1,9 @@
-export async function notifyCoach(title, body) {
+export async function notifyCoach(title, body, options = {}) {
   try {
     await fetch('/api/push-notify', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title, body })
+      body: JSON.stringify({ title, body, ...options })
     })
   } catch (err) {
     console.error('Coach push notification error:', err)
